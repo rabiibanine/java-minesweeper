d9e5f1ebdc706485c428608dc0f44c2d07b9051f
rabiibanine <rbanine@gmail.com>
2026-05-23 19:52:15 +0100

    feat: add GameView

--- src/main/java/com/rabiiyouness/minesweeper/view/screens/GameView.java
+++ src/main/java/com/rabiiyouness/minesweeper/view/screens/GameView.java
@@ -1,4 +1,26 @@
 package com.rabiiyouness.minesweeper.view.screens;
 
+import com.rabiiyouness.minesweeper.view.components.*;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
 public class GameView {
+
+    private BorderPane root;
+
+    public GameView () {
+//        GameTopBar gameTopBar = new GameTopBar();
+//        GameBoard gameBoard = new GameBoard();
+
+        root = new BorderPane();
+        // TEMP
+        root.setCenter(new Label("hello"));
+//        root.setTop(gameTopBar);
+//        root.setCenter(gameBoard);
+    }
+
+    public Parent getRoot() {
+        return root;
+    }
 }
