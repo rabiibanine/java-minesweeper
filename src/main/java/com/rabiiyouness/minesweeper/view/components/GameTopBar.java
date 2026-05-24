0f4a1c85acccf6d06926dfa4c4f35afd4c25b9fc
rabiibanine <rbanine@gmail.com>
2026-05-24 13:04:36 +0100

    feat: implement GameTopBar

--- src/main/java/com/rabiiyouness/minesweeper/view/components/GameTopBar.java
+++ src/main/java/com/rabiiyouness/minesweeper/view/components/GameTopBar.java
@@ -1,12 +1,59 @@
 package com.rabiiyouness.minesweeper.view.components;
 
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 
 public class GameTopBar {
 
+    HBox root;
+
+    public GameTopBar() {
+        this.root = new HBox();
+        root.setAlignment(Pos.CENTER_LEFT);
+
+        HBox leftSide = buildLeftSide();
+        HBox rightSide = buildRightSide();
+
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        root.getStyleClass().add("topbar");
+        root.getChildren().addAll(leftSide, spacer, rightSide);
+    }
+
     public Parent getComponent() {
-        HBox root = new HBox();
         return root;
+    }
+
+    public HBox buildLeftSide() {
+        HBox leftSide = new HBox();
+
+        Label logoLabel = new Label("MINESWEEPER PLUS");
+        logoLabel.setMaxHeight(Double.MAX_VALUE);
+        logoLabel.getStyleClass().add("topbar-logo");
+
+        leftSide.getChildren().addAll(logoLabel);
+        return leftSide;
+    }
+
+    public HBox buildRightSide() {
+        HBox rightSide = new HBox();
+
+        Button homeButton = new Button();
+        ImageView icon = new ImageView(new Image(getClass().getResourceAsStream("/icons/home.png")));
+        icon.setFitWidth(36);
+        icon.setFitHeight(36);
+        homeButton.setGraphic(icon);
+        homeButton.getStyleClass().add("topbar-icon-button");
+
+        rightSide.getChildren().addAll(homeButton);
+        return rightSide;
     }
 }
